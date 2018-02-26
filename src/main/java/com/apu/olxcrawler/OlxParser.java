93a5d3a15ffdafbe5f1bd5614d8d10bbee75d196
apu <pasha_anik@ukr.net>
2018-02-26 18:20:34 +0200

    Begin make olx crawler

---
+++ src/main/java/com/apu/olxcrawler/OlxParser.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.apu.olxcrawler;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
+/**
+ *
+ * @author apu
+ */
+public class OlxParser {
+    
+    private static final Log log = Log.getInstance();
+    private final Class classname = OlxParser.class;
+    
+    private final String OLX_HOST_URL = "https://www.olx.ua/";    
+    private final String OLX_PHONE_URL = "ajax/misc/contact/phone/";
+    
+    String requestPhoneFromUrl(String urlStr) {
+        String phoneResult = null;
+        
+        String idStr = this.getIDfromUrl(urlStr);
+        if(idStr == null)   
+                return null;
+        
+        String content;
+        List<Cookie> cookies = null;
+        
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+            HttpGet request = new HttpGet(urlStr);
+            HttpClientContext context = HttpClientContext.create();
+
+            try (CloseableHttpResponse response = httpClient.execute(request, context)) {
+                CookieStore cookieStore = context.getCookieStore();
+                cookies = cookieStore.getCookies();
+                HttpEntity entity = response.getEntity();
+                content = EntityUtils.toString(entity);
+            }            
+        } catch (Exception ex) {
+            log.debug(classname, ExceptionUtils.getStackTrace(ex));
+            return null;
+        } 
+        
+        BasicCookieStore cookieStore = new BasicCookieStore();
+        for(Cookie cookie:cookies) {
+            cookieStore.addCookie(cookie);
+        }
+        
+        try (CloseableHttpClient httpClient = HttpClientBuilder
+                    .create().setDefaultCookieStore(cookieStore).build()) {
+            
+            String token = getTokenFromContent(content);
+            if(token == null)   
+                    return null;
+            
+            String requestPhoneNumber = OLX_HOST_URL + 
+                                    OLX_PHONE_URL + idStr + "/?pt=" + token;
+            log.debug(classname, requestPhoneNumber);
+            
+            HttpGet request = new HttpGet(requestPhoneNumber);
+            request.addHeader(HttpHeaders.HOST, "www.olx.ua");
+            request.addHeader(HttpHeaders.REFERER, urlStr);
+            request.addHeader(HttpHeaders.ACCEPT, "text/html,application/xhtml+xml,"
+                    + "application/xml;q=0.9,image/webp,image/apng,*/*");
+            request.addHeader(HttpHeaders.ACCEPT_LANGUAGE, "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7");
+            request.addHeader(HttpHeaders.ACCEPT_ENCODING, "Accept-Encoding: gzip, deflate, br");
+            request.addHeader(HttpHeaders.CONNECTION, "keep-alive");
+            request.addHeader("X-Requested-With", "XMLHttpRequest");
+            request.addHeader(HttpHeaders.USER_AGENT, "Mozilla/5.0 (Windows NT 6.1; Win64; x64) "
+                    + "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36");
+            
+            try (CloseableHttpResponse response = httpClient.execute(request)) {                
+                HttpEntity entity = response.getEntity();
+                phoneResult = EntityUtils.toString(entity);            
+            } 
+        } catch (Exception ex) {
+            log.debug(classname, ExceptionUtils.getStackTrace(ex));
+            return null;
+        }
+        
+        return phoneResult;
+    }
+    
+    private String getTokenFromContent(String content) {
+        String regExpUrl = "(.*)var phoneToken = \\'(.*)\\';(.*)";    
+        Pattern pattern = Pattern.compile(regExpUrl, Pattern.DOTALL);  
+        
+        Matcher matcher = pattern.matcher(content);        
+        if(matcher.matches() == false) return null;
+        
+        String tokenStartPattern = "var phoneToken = '";
+        String tokenEndPattern = "';";
+        int tokenStartPosition = content.indexOf(tokenStartPattern) + 
+                                        tokenStartPattern.length();
+        int tokenEndPosition = content.indexOf(tokenEndPattern, 
+                                        tokenStartPosition + 1);
+
+        return content.substring(tokenStartPosition, tokenEndPosition);        
+    }
+    
+    private String getIDfromUrl(String url) {
+        String regExpUrl = ".+\\-ID(.*).html(.*)";        
+        Pattern pattern = Pattern.compile(regExpUrl, Pattern.DOTALL);        
+        Matcher matcher = pattern.matcher(url);        
+        if(matcher.matches() == false) return null;
+        
+        String startPattern = "-ID";
+        String endPattern = ".html";
+        int startPosition = url.indexOf(startPattern) + startPattern.length();
+        int endPosition = url.indexOf(endPattern, startPosition + 1);
+        return url.substring(startPosition, endPosition);
+    }
+    
+    public static void main(String[] args) {
+        String urlStr = "https://www.olx.ua/obyavlenie/learn-version-control-with-"
+            + "git-raspredelennaya-sistema-upravleniya-vers-IDya9jS.html#5b61bf5b91";
+        OlxParser parser = new OlxParser();
+        
+        String idStr = parser.getIDfromUrl(urlStr);
+        System.out.println(idStr);
+        
+        String phoneStr = parser.requestPhoneFromUrl(urlStr);
+        System.out.println(phoneStr);
+    }
+    
+}
